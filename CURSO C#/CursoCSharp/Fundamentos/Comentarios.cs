b41e2146dbbbb2e956d70b3ae400ee21a931e9e3
CarlosSMori <lester_novo@hotmail.com>
2020-03-12 15:33:25 -0300

    Commit

---
+++ CURSO C#/CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class Comentarios {
+        public static void Executar() {
+
+            //cuidado com comentarios desnecessários
+            Console.WriteLine("Codigo claro é sempre melhor!");
+
+            /// <summary>
+            /// 
+            /// </summary>
+            Console.WriteLine("O C# tem o XML Comments");
+
+            /* 
+             * Esse é comentario
+             * de multiplas linhas...
+             */
+            Console.WriteLine("Comentarios de multiplas linhas");
+
+
+            Console.WriteLine("Comando CRT + k , CRT + C comenta seleção");
+            Console.WriteLine("Comando CRT + k , CRT + u descomenta seleção");
+        }
+    }
+}
---
+++ CURSO C#/CursoCSharp/Fundamentos/VariaveisEConstates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class VariaveisEConstates {
+        public static void Executar() {
+            //area da circunferencia
+            double raio = 4.5;
+            const double PI = 3.14;
+
+            raio = 5.5;
+            //PI = 3.1415;
+
+            double area = PI * raio * raio;
+            Console.WriteLine(area);
+            Console.WriteLine("Area é :" + area);
+
+            //Exemplo de variavel Booleana.
+            bool estaChovendo = true;
+            Console.WriteLine("Está chovendo :" + estaChovendo);
+
+
+            byte idade = 38;
+            Console.WriteLine("A idade é:" + idade);
+
+            sbyte saldoDeGols = sbyte.MinValue;
+            Console.WriteLine("Saldo de gols é: " + saldoDeGols);
+
+            short salario = short.MaxValue;
+            Console.WriteLine("O sálario é: " + salario);
+        }
+    }
+}
